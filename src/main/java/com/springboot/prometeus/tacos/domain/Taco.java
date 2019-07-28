61450e113c4e4e4a5a9cd24fe9bb3a19145efde5
iprometeus <iprometeus2016@gmail.com>
2019-07-28 22:09:29 +0300

    Added design page and controller.

---
+++ src/main/java/com/springboot/prometeus/tacos/domain/Taco.java
@@ -0,0 +1,12 @@
+package com.springboot.prometeus.tacos.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<String> ingredients;
+}
