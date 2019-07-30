0a06be8b265f775b1e4d481c8148025840d94959
iprometeus <iprometeus2016@gmail.com>
2019-07-30 23:18:24 +0300

    Added jdbcTacoRepo

--- src/main/java/com/springboot/prometeus/tacos/domain/Order.java
+++ src/main/java/com/springboot/prometeus/tacos/domain/Order.java
@@ -3,7 +3,9 @@ package com.springboot.prometeus.tacos.domain;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
@@ -34,9 +36,15 @@ public class Order {
     private String ccNumber;
 
     @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-             message = "Must be formatted MM/YY")
+            message = "Must be formatted MM/YY")
     private String ccExpiration;
 
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
 }
--- src/main/java/com/springboot/prometeus/tacos/web/DesignTacoController.java
+++ src/main/java/com/springboot/prometeus/tacos/web/DesignTacoController.java
@@ -1,23 +1,22 @@
 package com.springboot.prometeus.tacos.web;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.springboot.prometeus.tacos.data.IngredientRepository;
+import com.springboot.prometeus.tacos.data.TacoRepository;
 import com.springboot.prometeus.tacos.domain.Ingredient;
 import com.springboot.prometeus.tacos.domain.Ingredient.Type;
+import com.springboot.prometeus.tacos.domain.Order;
 import com.springboot.prometeus.tacos.domain.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -27,10 +26,25 @@ public class DesignTacoController {
 
     private final IngredientRepository ingredientRepo;
 
+    private TacoRepository designRepo;
+
     @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepo) {
+    public DesignTacoController(
+            IngredientRepository ingredientRepo,
+            TacoRepository designRepo) {
 
         this.ingredientRepo = ingredientRepo;
+        this.designRepo = designRepo;
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
     }
 
     @GetMapping
@@ -43,7 +57,7 @@ public class DesignTacoController {
         Type[] types = Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
-                               filterByType(ingredients, type));
+                    filterByType(ingredients, type));
         }
         model.addAttribute("design", new Taco());
 
@@ -51,22 +65,21 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
-
+    public String processDesign(
+            @Valid Taco design, Errors errors,
+            @ModelAttribute Order order) {
         if (errors.hasErrors()) {
             return "design";
         }
-        // Save the taco design...
-        // Save the taco design...
-        // We'll do this in chapter 3
-        log.info("Processing design: " + design);
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
         return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 
         return ingredients.stream()
-                          .filter(i -> type.equals(i.getType()))
-                          .collect(Collectors.toList());
+                .filter(i -> type.equals(i.getType()))
+                .collect(Collectors.toList());
     }
 }
